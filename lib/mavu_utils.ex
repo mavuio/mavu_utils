29efc945b8c03c951799e89fe98c6f5b164097e3
Manfred Wuits <manfred@monochrom.at>
2021-02-26 13:26:27 +0100

    first commit

---
+++ lib/mavu_utils.ex
@@ -0,0 +1,93 @@
+defmodule MavuUtils do
+  @moduledoc """
+    utility helpers by Manfred Wuits
+  """
+
+  defmacro pipe_when(left, condition, fun) do
+    quote do
+      left = unquote(left)
+
+      if unquote(condition),
+        do: left |> unquote(fun),
+        else: left
+    end
+  end
+
+  def if_empty(val, default_val) do
+    if present?(val) do
+      val
+    else
+      default_val
+    end
+  end
+
+  def if_nil(val, default_val) do
+    if is_nil(val) do
+      default_val
+    else
+      val
+    end
+  end
+
+  def to_int(val) do
+    case val do
+      val when is_integer(val) ->
+        val
+
+      val when is_binary(val) ->
+        Integer.parse(val)
+        |> case do
+          {val, ""} -> val
+          _ -> nil
+        end
+
+      val when is_float(val) ->
+        Kernel.round(val)
+
+      %Decimal{} = val ->
+        val |> Decimal.round() |> Decimal.to_integer()
+
+      nil ->
+        nil
+    end
+  end
+
+  def present?(term) do
+    !Blankable.blank?(term)
+  end
+
+  def empty?(term) do
+    Blankable.blank?(term)
+  end
+
+  def false?(false), do: true
+  def false?("false"), do: true
+  def false?(-1), do: true
+  def false?(0), do: true
+  def false?("0"), do: true
+  def false?("-1"), do: true
+
+  def false?(term) do
+    empty?(term)
+  end
+
+  def true?(true), do: true
+  def true?("true"), do: true
+  def true?(1), do: true
+  def true?("1"), do: true
+
+  def true?(term) do
+    !false?(term)
+  end
+
+  require Logger
+
+  def log(data, msg \\ "", level) when is_atom(level) do
+    Logger.log(
+      level,
+      msg <> " " <> inspect(data, printable_limit: :infinity, limit: 50, pretty: true)
+    )
+
+    data
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule MavuUtils.MixProject do
+  use Mix.Project
+
+  @version "0.1.0"
+  def project do
+    [
+      app: :mavu_utils,
+      version: @version,
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:decimal, "~> 2.0"},
+      {:blankable, "~> 1.0"}
+    ]
+  end
+end
