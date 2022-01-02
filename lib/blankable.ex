913938a878e612bbf39f2830a241c8a14615b8c2
Manfred Wuits <manfred@mavu.io>
2022-01-02 21:27:03 +0100

    fixed blankable implementation

--- lib/blankable.ex
+++ lib/blankable.ex
@@ -21,29 +21,29 @@ defprotocol MavuUtils.Blankable do
   def blank?(term)
 end
 
-defimpl Blankable, for: List do
+defimpl MavuUtils.Blankable, for: List do
   def blank?([]), do: true
   def blank?(_), do: false
 end
 
-defimpl Blankable, for: Map do
+defimpl MavuUtils.Blankable, for: Map do
   # Keep in mind we could not pattern match on %{} because it matches on all
   # maps. We can however check if the size is zero (and size is a fast
   # operation).
   def blank?(map), do: map_size(map) == 0
 end
 
-defimpl Blankable, for: Tuple do
+defimpl MavuUtils.Blankable, for: Tuple do
   def blank?({}), do: true
   def blank?(_), do: false
 end
 
-defimpl Blankable, for: BitString do
+defimpl MavuUtils.Blankable, for: BitString do
   def blank?(""), do: true
   def blank?(_), do: false
 end
 
-defimpl Blankable, for: Any do
+defimpl MavuUtils.Blankable, for: Any do
   def blank?(nil), do: true
   def blank?(false), do: true
   def blank?(atom) when is_atom(atom), do: false
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule MavuUtils.MixProject do
   use Mix.Project
 
-  @version "0.1.8"
+  @version "0.1.9"
   def project do
     [
       app: :mavu_utils,
